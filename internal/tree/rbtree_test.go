b42441ce4cd8ab4cba0e471151e2774ccd502f75
Gabriel Saiago <grsaiago@gmail.com>
2025-11-14 23:13:30 -0300

    add test for insert on tree root

--- internal/tree/rbtree_test.go
+++ internal/tree/rbtree_test.go
@@ -12,3 +12,16 @@ func TestSearchInEmptyTree(t *testing.T) {
 
 	assert.NotNil(t, err, "should've returned a ErrNotFound")
 }
+
+func TestInsertSearchOnRoot(t *testing.T) {
+	tree := RBTree[int]{}
+
+	err := tree.Insert(10)
+	assert.Nil(t, err, "insert should have not errored")
+
+	found, err := tree.Search(10)
+	assert.Nil(t, err, "search should have not errored")
+	assert.NotNil(t, found, "search should have not found something")
+	assert.Equal(t, found, tree.root, "what was found is the tree's root")
+
+}
