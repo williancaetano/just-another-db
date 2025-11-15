a946364ef0b50ef89d00430cad3799ba8212f965
Gabriel Saiago <grsaiago@gmail.com>
2025-11-14 21:09:57 -0300

    add simple search implementation

--- internal/tree/rbtree.go
+++ internal/tree/rbtree.go
@@ -6,7 +6,7 @@ import (
 
 // TODO: Consider implementing a trait Comparable
 type RBTree[T cmp.Ordered] struct {
-	root *Node[T]
+	root  *Node[T]
 	count uint32
 }
 
@@ -14,22 +14,31 @@ func NewRBTree[T cmp.Ordered]() *RBTree[T] {
 	return &RBTree[T]{}
 }
 
-func (t *RBTree[T]) Search(data T) (T, error) {
-	if (t.root.data == data) {
-		return t.root.data, nil
+func (t *RBTree[T]) Search(data T) (*Node[T], error) {
+	if t.root.data == data {
+		return t.root, nil
 	}
-	
-	
-	var zero T
-	return zero, ErrNodeNotFound
+
+	curr := t.root
+	for curr != nil {
+		if curr.data == data {
+			return curr, nil
+		} else if data < curr.data {
+			curr = curr.left
+		} else {
+			curr = curr.right
+		}
+	}
+
+	return nil, ErrNodeNotFound
 }
 
 func (t *RBTree[T]) Insert(data T) error {
 	node := NewNode(data)
 
-	if (t.root == nil) {
+	if t.root == nil {
 		t.root = node
-		return nil 
+		return nil
 	}
 
 	return nil
@@ -42,5 +51,3 @@ func (t *RBTree[T]) Delete(data T) error {
 func (t *RBTree[T]) String() string {
 	return ""
 }
-
-
